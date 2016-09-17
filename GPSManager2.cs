93a1e9ed4e90c9cab0775d17ed092ad67b457deb
WooHee_Lee <woohee_lee@WooHee-Leeui-MacBook-Pro.local>
2016-09-18 06:20:54 +0900

    위도 경도 주고 받기 까지

---
+++ GPSManager2.cs
@@ -0,0 +1,245 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum LocationState{
+	Disabled,
+	TimedOut,
+	Failed,
+	Enabled
+}
+
+public class GPSManager2 : MonoBehaviour {
+
+	public static int SCREEN_DENSITY;
+	private GUIStyle debugStyle;
+	// Approximate radius of the earth (in kilometers)
+	const float EARTH_RADIUS = 6371;
+	private LocationState state;
+	// Position on earth (in degrees)
+	private float latitude;
+	private float longitude;
+	// Distance walked (in meters)
+	private float distance;
+	// Coins obtained (1 for every 100 meters walked)
+	private int coins;
+
+	//PhotonView 컴포넌트를 할당할 변수
+	private PhotonView pv = null;
+
+	private float currLati;
+	private float currLongi;
+	//public int currUserID;
+	void Awake()
+	{
+		// 초기화
+		pv = GetComponent<PhotonView>();
+		// 데이터 전송 타입을 설정 (변경 사항이 있을때만 전송)
+		pv.synchronization = ViewSynchronization.Unreliable;
+		// PhotonView Observed Components 속성에 TankMove 스크립트를 연결
+		pv.ObservedComponents[0] = this;
+		currLati = 0.0f;
+		currLongi = 0.0f;
+		//currUserID = PhotonNetwork.player.ID;
+		
+	}
+
+	// Use this for initialization
+	IEnumerator Start () {
+		if(Screen.dpi > 0f){
+			SCREEN_DENSITY = (int)(Screen.dpi / 160f);
+		}else{
+			SCREEN_DENSITY = (int)(Screen.currentResolution.height / 600);
+		}
+		debugStyle = new GUIStyle ();
+		debugStyle.fontSize = 16 * SCREEN_DENSITY;
+		debugStyle.normal.textColor = Color.white;
+
+		state = LocationState.Disabled;
+		latitude = 0f;
+		longitude = 0f;
+		distance = 0f;
+		coins = 0;
+
+
+		if(Input.location.isEnabledByUser){
+			Input.location.Start();
+			int waitTime = 15;
+			while(Input.location.status == LocationServiceStatus.Initializing && waitTime > 0){
+				yield return new WaitForSeconds(1);
+				waitTime--;
+			}
+			if(waitTime == 0){
+				state = LocationState.TimedOut;
+			}else if(Input.location.status == LocationServiceStatus.Failed){
+				state = LocationState.Failed;
+			}else{
+				state = LocationState.Enabled;
+				latitude = Input.location.lastData.latitude;
+				longitude = Input.location.lastData.longitude;
+			}
+		}
+	}
+
+	IEnumerator OnApplicationPause(bool pauseState){
+		if(pauseState){
+			Input.location.Stop();
+			state = LocationState.Disabled;
+		}else{
+			Input.location.Start();
+			int waitTime = 15;
+			while(Input.location.status == LocationServiceStatus.Initializing && waitTime > 0){
+				yield return new WaitForSeconds(1);
+				waitTime--;
+			}
+			if(waitTime == 0){
+				state = LocationState.TimedOut;
+			}else if(Input.location.status == LocationServiceStatus.Failed){
+				state = LocationState.Failed;
+			}else{
+				state = LocationState.Enabled;
+				latitude = Input.location.lastData.latitude;
+				longitude = Input.location.lastData.longitude;
+			}
+		}
+	}
+
+	void OnGUI(){
+		Rect guiBoxRect = new Rect(40, 20, Screen.width-80, Screen.height-40);
+
+		GUI.skin.box.fontSize = 32 * SCREEN_DENSITY;
+		GUI.Box (guiBoxRect, "GPS Demo");
+
+		float buttonHeight = guiBoxRect.height / 7;
+
+		switch(state){
+		case LocationState.Enabled:
+			GUILayout.Label("latitude: "+latitude.ToString(),debugStyle,GUILayout.Width(Screen.width / 4));
+			GUILayout.Label("longitude: "+longitude.ToString(),debugStyle,GUILayout.Width(Screen.width / 4));
+
+			Rect distanceTextRect = new Rect(guiBoxRect.x+40, guiBoxRect.y + guiBoxRect.height/3,
+				guiBoxRect.width-80, buttonHeight);
+
+			GUI.skin.label.fontSize = 40 * SCREEN_DENSITY;
+			GUI.skin.label.alignment = TextAnchor.UpperCenter;
+			GUI.Label(distanceTextRect, "Distance walked: "+distance.ToString()+"m");
+
+			Rect coinsTextRect = new Rect(guiBoxRect.x+40, guiBoxRect.y + guiBoxRect.height*2/3,
+				guiBoxRect.width-80, buttonHeight);
+
+			GUI.skin.label.fontSize = 40 * SCREEN_DENSITY;
+			GUI.skin.label.alignment = TextAnchor.UpperCenter;
+			GUI.Label(coinsTextRect, "Coins: "+coins.ToString()+"g");
+			break;
+		case LocationState.Disabled:
+			Rect disabledTextRect = new Rect(guiBoxRect.x+40, guiBoxRect.y + guiBoxRect.height/2,
+				guiBoxRect.width-80, buttonHeight*2);
+
+			GUI.skin.label.fontSize = 40 * SCREEN_DENSITY;
+			GUI.skin.label.alignment = TextAnchor.UpperCenter;
+			GUI.Label(disabledTextRect, "GPS is disabled. GPS must be enabled\n" +
+				"in order to use this application.");
+			break;
+		case LocationState.Failed:
+			Rect failedTextRect = new Rect(guiBoxRect.x+40, guiBoxRect.y + guiBoxRect.height/2,
+				guiBoxRect.width-80, buttonHeight*2);
+
+			GUI.skin.label.fontSize = 40 * SCREEN_DENSITY;
+			GUI.skin.label.alignment = TextAnchor.UpperCenter;
+			GUI.Label(failedTextRect, "Failed to initialize location service.\n" +
+				"Try again later.");
+			break;
+		case LocationState.TimedOut:
+			Rect timeOutTextRect = new Rect(guiBoxRect.x+40, guiBoxRect.y + guiBoxRect.height/2,
+				guiBoxRect.width-80, buttonHeight*2);
+
+			GUI.skin.label.fontSize = 40 * SCREEN_DENSITY;
+			GUI.skin.label.alignment = TextAnchor.UpperCenter;
+			GUI.Label(timeOutTextRect, "Connection timed out. Try again later.");
+			break;
+		}
+	}
+
+	// The Haversine formula
+	// Veness, C. (2014). Calculate distance, bearing and more between
+	//	Latitude/Longitude points. Movable Type Scripts. Retrieved from
+	//	http://www.movable-type.co.uk/scripts/latlong.html
+	float Haversine(ref float lastLatitude,ref float lastLongitude){
+		float newLatitude = Input.location.lastData.latitude;
+		float newLongitude = Input.location.lastData.longitude;
+		float deltaLatitude = (newLatitude - lastLatitude) * Mathf.Deg2Rad;
+		float deltaLongitude = (newLongitude - lastLongitude) * Mathf.Deg2Rad;
+		float a = Mathf.Pow(Mathf.Sin(deltaLatitude / 2),2) +
+			Mathf.Cos(lastLatitude * Mathf.Deg2Rad) * Mathf.Cos(newLatitude * Mathf.Deg2Rad) *
+			Mathf.Pow(Mathf.Sin(deltaLongitude / 2),2);
+		lastLatitude = newLatitude;
+		lastLongitude = newLongitude;
+		float c = 2 * Mathf.Atan2(Mathf.Sqrt(a),Mathf.Sqrt(1-a));
+		return EARTH_RADIUS * c;
+	}
+
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		//Debug.Log("나의 userID = " + currUserID + "입니다. 서버야 일하고 있니?");
+		//Debug.Log("나의 userName = " + PhotonNetwork.player.name + "입니다. 서버야 일하고 있니?");
+		if(stream.isWriting)
+		{
+//			Debug.Log("발신 PhotonNetwork.isMasterClient = " + PhotonNetwork.isMasterClient);
+			if(PhotonNetwork.isMasterClient)
+			{
+//				Debug.Log("마스터 입장한 방 이름 = " + PhotonNetwork.room.name);
+				stream.SendNext("화이팅");
+			}
+			else
+			{
+				// 마스터 클라이언트가 아니면
+				//Debug.Log("아이폰아 데이터 보내고 있나?");
+				//stream.SendNext("우희야");
+				//Debug.Log("손님 입장한 방 이름 = " + PhotonNetwork.room.name);
+	
+				//Debug.Log("아이폰 정보를 보낸다. 경도 = " + latitude);
+				// 송신
+				// 자신의 위도 경도 값을 보냄
+				stream.SendNext(latitude);
+				stream.SendNext(longitude);
+				
+				//stream.SendNext(pv.viewID);
+			}
+		}else
+		{
+//			Debug.Log("수신 PhotonNetwork.isMasterClient = " + PhotonNetwork.isMasterClient);
+			// 수신
+			// 방장만 수신 함.
+			if(PhotonNetwork.isMasterClient)
+			{
+//				Debug.Log("아 제발!!");
+				currLati = (float)stream.ReceiveNext();
+				currLongi = (float)stream.ReceiveNext();
+				//currUserID = (int)stream.ReceiveNext();
+//				Debug.Log("아이폰 정보를 받았다. 경도 = " + latitude);
+
+				Debug.Log("currLati = " + currLati);
+				Debug.Log("currLongi = " + currLongi);
+				
+				//Debug.Log("currUserID = " + currUserID);
+			}
+			else{
+			// 마스터 클라이언트가 아니면
+				Debug.Log((string)stream.ReceiveNext());
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(!PhotonNetwork.isMasterClient)
+		{
+			if(state == LocationState.Enabled){
+				float deltaDistance = Haversine(ref latitude,ref longitude) * 1000f;
+				if(deltaDistance > 0f){
+					distance += deltaDistance;
+					coins = (int)(distance / 100f);
+				}
+			}
+		}
+	}
+}
---
+++ PhotonInit2.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class PhotonInit2 : MonoBehaviour {
+	//App의 버전 정보
+	public string version = "v1.0";
+
+	public const float maxLatitude = 37.2918f;
+	public const float minLatitude = 37.2908f;
+	public const float maxLongitude = 127.1442f;
+	public const float minLongitude = 127.1396f;
+
+	//private PhotonView m_pv;
+
+	public struct ItemPos
+	{
+		// N 위도
+		private float[] latitude;
+		// E 경도
+		private float[] longitude;
+
+		// 생성자
+		public ItemPos(int maxSize)
+		{
+			latitude = new float[maxSize];
+			longitude = new float[maxSize];
+		}
+		public void initPos()
+		{
+			for(int i=0; i< latitude.Length; i++)
+			{
+				latitude[i] = Random.Range(minLatitude, maxLatitude);
+				longitude[i] = Random.Range(minLongitude, maxLongitude);
+			}
+		}
+
+		public float[] getLatitude()
+		{
+			return latitude;
+		}
+		public float[] getLongitude()
+		{
+			return longitude;
+		}
+
+	}
+
+	public static ItemPos itemPos;
+
+	void Awake()
+	{
+		//m_pv = GameObject.Find("GPSMgr").GetComponent<PhotonView>();
+
+		
+		//포톤 클라우드에 접속
+		PhotonNetwork.ConnectUsingSettings(version);
+	}
+
+	//포톤 클라우드에 정상적으로 접속한 후 로비에 입장하면 호출되는 콜백 함수
+	void OnJoinedLobby()
+	{
+		Debug.Log("Entered Lobby!");
+		// 일단 룸에 입장해 본다.
+		PhotonNetwork.JoinRoom("MyRoom");
+		/*
+		// 로비에 입장하고 나서 룸이 만약 없으면
+		if(PhotonNetwork.room == null)
+		{
+			
+		}else{
+			Debug.Log("myRoom 에 입장 합니다.");
+			// room이 있으면
+			PhotonNetwork.JoinRoom("MyRoom");
+		}
+		*/
+	}
+
+	// 룸 입장에 실패할 경우 호출되는 콜백 함수
+	void OnPhotonJoinRoomFailed()
+	{
+		Debug.Log("myRoom 을 만듭니다.");
+		//RoomOptions roomOption = new RoomOptions();
+		// 룸에서 플레이어들의 UserID를 알 수 있도록 하는 옵션
+		//roomOption.publishUserId = true;
+		// 룸에 정상적으로 접속했으면 룸을 만든다.
+		// 룸을 생성한 플레이어는 자동으로 룸에 입장한다.
+		//PhotonNetwork.CreateRoom("MyRoom", roomOption, null);
+		PhotonNetwork.CreateRoom("MyRoom");
+	}
+
+	// 룸에 입장하면 호출되는 콜백 함수
+	void OnJoinedRoom()
+	{				
+		Debug.Log("Enter Room");
+		// GPS매니저 생성
+		CreateGPSMgr();
+		//Debug.Log("나의 currUserID = " + m_photonInit.currUserID);
+		// 방장인 경우
+		if(PhotonNetwork.isMasterClient)
+		{
+//			Debug.Log("마스터 입장한 방 이름 = " + PhotonNetwork.room.name);
+//			Debug.Log("마스터 클라이언트 초기화");
+			// 아이템 위치 메모리 할당
+			itemPos  = new ItemPos(10);
+			// 아이템 위치 초기화
+			itemPos.initPos();
+
+			for(int i=0; i<10; i++)
+			{
+				Debug.Log("Latitude = " + itemPos.getLatitude()[i]);
+				Debug.Log("Longitude = " + itemPos.getLongitude()[i]);
+			}
+		}
+	}
+
+	// GPSMgr를 생성하는 함수
+	void CreateGPSMgr()
+	{
+		PhotonNetwork.Instantiate("GPSMgr", new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity, 0);
+	}
+
+	void OnGUI()
+	{
+		//화면 좌측 상단에 접속 과 정 에 대 한 로 그 를 출 력
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+}
